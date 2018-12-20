339577cad3f004286c72c8f81fe4ce1fd1196316
juanchavez <miguel_trinch@hotmail.com>
2018-12-20 00:44:14 -0500

    Cambios

--- server_api/settings.py
+++ server_api/settings.py
@@ -77,13 +77,23 @@ WSGI_APPLICATION = 'server_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "moteles",
+        "USER": "postgres",
+        "HOST": "localhost",
+        "PASSWORD": "123456",
+        "PORT": 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
